884e0c421b4e99332d874065eefb9a1d682c664b
Jesús Ávila <jesusam1995@hotmail.com>
2021-04-06 21:13:00 +0200

    Se añade nueva escena para seleccionar Barcos. Se añade funcionalidad de manejo para navegar entre escenas (selector de niveles, vuelta atras, instrucciones, etc.)

---
+++ Assets/ManagerScene.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerScene : MonoBehaviour
+{
+    public int nivel;
+    public int barco;
+
+    private string NIVEL_NOMBRE = "Nivel";
+    // private string BARCO_NOMBRE = "Barco";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        LoadData();
+    }
+
+
+    private void OnDestroy()
+    {
+        SaveData();
+    }
+
+    // Metodos 
+
+    public void CambiarEscena(string escena)
+    {
+        SceneManager.LoadScene(escena);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+    public void SeleccionNivel(int nivel)
+    {
+        print("Nivel anterior:" + this.nivel);
+        this.nivel = nivel;
+        print("Nivel asignado:" + this.nivel);
+
+        
+
+        CambiarEscena("Barcos");
+    }
+
+    private void SaveData()
+    {
+        PlayerPrefs.SetInt(NIVEL_NOMBRE, this.nivel);
+    }
+
+    private void LoadData()
+    {
+        this.nivel = PlayerPrefs.GetInt(NIVEL_NOMBRE, 1);
+    }
+}
