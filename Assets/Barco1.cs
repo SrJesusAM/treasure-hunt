2f8879a7e8aa695797e9d272e44f33ee80c818f9
Jesús Ávila <jesusam1995@hotmail.com>
2021-04-28 22:17:37 +0200

    Se añade un image tarjet de prueba y un asset de barco que autodetecta y muestra ese barco.

---
+++ Assets/Barco1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Barco1 : MonoBehaviour, ITrackableEventHandler {
+
+    // Variables
+    protected TrackableBehaviour mTrackableBehaviour;
+
+
+    void Start() {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,   TrackableBehaviour.Status newStatus) {
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+            print("DETECTA");
+        } else if (previousStatus == TrackableBehaviour.Status.TRACKED && newStatus == TrackableBehaviour.Status.NO_POSE) {
+            print("PIERDE");
+        } else {
+            print("COMIENZO");
+        }
+    }
+
+}
