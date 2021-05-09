3d5dbbb6204415336afe5a0e6e4559a951bc1e35
Jesús Ávila <jesusam1995@hotmail.com>
2021-05-09 14:26:30 +0200

    Se añaden miniaturas de los barcos y se realiza la carga del barco seleccionado en los niveles.

---
+++ Assets/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public Sprite barco1;
+    public Sprite barco2;
+    public Sprite barco3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject gameManager = GameObject.Find("GameManagerLevels");
+        ManagerScene managerScene = gameManager.GetComponent<ManagerScene>();
+
+        print("Barco elegido scene"+ managerScene.barco);
+
+        switch (managerScene.barco)
+        {
+
+            case 1:
+                GetComponent<Image>().sprite = barco1;
+                break;
+            case 2:
+                GetComponent<Image>().sprite = barco2;
+                break;
+            case 3:
+                GetComponent<Image>().sprite = barco3;
+                break;
+            default:
+                GetComponent<Image>().sprite = barco1;
+                break;
+        }
+
+
+    }        
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
