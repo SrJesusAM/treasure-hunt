6bbd5b77e15fc80a9cf06612c431f4e4a8a3249b
Jesús Ávila <jesusam1995@hotmail.com>
2021-05-09 21:53:21 +0200

    Se añade funcionalidad controlador de flechas. Se añade panel de soluciones, y botones de repetir movimiento y finalizar.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -10,6 +10,15 @@ public class GameController : MonoBehaviour
     public Sprite barco2;
     public Sprite barco3;
 
+    public Sprite flechaArriba;
+    public Sprite flechaAbajo;
+    public Sprite flechaDerecha;
+    public Sprite flechaIzquierda;
+
+    public Sprite vacio;
+
+    private int contador = 0;
+    private string[] solucion = new string[5];
 
     // Start is called before the first frame update
     void Start()
@@ -17,28 +26,101 @@ public class GameController : MonoBehaviour
         GameObject gameManager = GameObject.Find("GameManagerLevels");
         ManagerScene managerScene = gameManager.GetComponent<ManagerScene>();
 
+        GameObject barcoObject = GameObject.Find("Barco");
         print("Barco elegido scene"+ managerScene.barco);
 
         switch (managerScene.barco)
         {
 
             case 1:
-                GetComponent<Image>().sprite = barco1;
+                barcoObject.GetComponent<Image>().sprite = barco1;
                 break;
             case 2:
-                GetComponent<Image>().sprite = barco2;
+                barcoObject.GetComponent<Image>().sprite = barco2;
                 break;
             case 3:
-                GetComponent<Image>().sprite = barco3;
+                barcoObject.GetComponent<Image>().sprite = barco3;
                 break;
             default:
-                GetComponent<Image>().sprite = barco1;
+                barcoObject.GetComponent<Image>().sprite = barco1;
                 break;
         }
 
 
     }        
 
+
+    public void movimiento(string flecha)
+    {
+        print(contador);
+
+        if (contador < 5)
+        {
+            contador++;
+            string imgSolPanelName = "imgSol" + contador;
+
+            GameObject imgSolObj = GameObject.Find(imgSolPanelName);
+            switch (flecha)
+            {
+
+                case "ABAJO":
+                    imgSolObj.GetComponent<Image>().sprite = flechaAbajo;
+                    solucion[contador - 1] = "ABAJO";
+                    break;
+                case "ARRIBA":
+                    imgSolObj.GetComponent<Image>().sprite = flechaArriba;
+                    solucion[contador - 1] = "ARRIBA";
+                    break;
+                case "DERECHA":
+                    imgSolObj.GetComponent<Image>().sprite = flechaDerecha;
+                    solucion[contador - 1] = "DERECHA";
+                    break;
+                default:
+                    imgSolObj.GetComponent<Image>().sprite = flechaIzquierda;
+                    solucion[contador - 1] = "IZQUIERDA";
+                    break;
+            }
+
+
+            if (contador >= 5)
+            {
+                contador = 5;
+            }
+            
+        }
+    }
+
+    public void borrarMovimientoAnterior()
+    {
+
+        if (contador > 0)
+        {
+            string imgSolPanelName = "imgSol" + contador;
+            GameObject imgSolObj = GameObject.Find(imgSolPanelName);
+
+            imgSolObj.GetComponent<Image>().sprite = vacio;
+            solucion[contador - 1] = "";
+
+            contador--;
+            if (contador <= 0)
+            {
+                contador = 0;
+            }
+            
+        }
+
+        
+    }
+
+    public void finalizar()
+    {
+
+        for (int i =0; i < contador; i++)
+        {
+            print("i:"+i +" - " + solucion[i]);
+        } 
+
+    }
 
 
     // Update is called once per frame
